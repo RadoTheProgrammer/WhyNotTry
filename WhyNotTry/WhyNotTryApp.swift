4626890ffd6626d81e6360a2a98292a1ec6d3b35
RadoTheProgrammer <rado@arazakar.com>
2024-02-27 18:52:29 +0100

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Alain Razakarivony on 27.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
