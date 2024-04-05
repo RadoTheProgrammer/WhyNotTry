c27922a98a8df97b1f1ba61c816d00ecf9ccd332
RadoTheProgrammer <rado@arazakar.com>
2024-04-05 13:02:49 +0200

    created project

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -3,21 +3,56 @@
 //  WhyNotTry
 //
 //  Created by Alain Razakarivony on 27.02.24.
-//
-
+// https://www.swift.org/getting-started/swiftui/
+// https://developer.apple.com/tutorials/swiftui
 import SwiftUI
 
 struct ContentView: View {
+    
+
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    @State private var selected = "Archery"
+    
+    @State private var id = 1
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Why not try ...")
+                .font(.largeTitle.bold())
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white))
+                Text("\(selected)")
+                    .font(.title)
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try again") {
+                withAnimation (.easeInOut(duration: 1)){
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
+        
     }
+
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
